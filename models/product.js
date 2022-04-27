fda915af2eff301eb8820070720bb2db94a6bf79
Kaylee Guise <guisekayl@gmail.com>
2022-04-26 22:22:19 -0500

    Added PUT, GET, GET by id, and DELETE endpoints; added Postman responses and collection.

--- models/product.js
+++ models/product.js
@@ -1,13 +1,24 @@
 const mongoose = require("mongoose");
+const Joi = require("joi");
 
 const productSchema = new mongoose.Schema({
-    name: {type: String, required: true, minlength: 2, maxlength: 255},
-    description: {type: String, required: true, minlength: 2},
-    category: {type: String, required: true, minlength: 2, maxlength: 255},
-    price: {type: Number, required: true},
-    dateAdded: {type: Date, default: Date.now()}
-})
+  name: { type: String, required: true, minlength: 2, maxlength: 255 },
+  description: { type: String, required: true, minlength: 2 },
+  category: { type: String, required: true, minlength: 2, maxlength: 255 },
+  price: { type: Number, required: true },
+  dateAdded: { type: Date, default: Date.now() },
+});
+
+function validateProduct(product) {
+  const schema = Joi.object({
+    name: Joi.string().min(2).max(255).required(),
+    description: Joi.string().min(2).required(),
+    category: Joi.string().min(2).max(255).required(),
+    price: Joi.number().required(),
+  });
+  return schema.validate(product);
+}
 
 const Product = mongoose.model("Product", productSchema);
 
-module.exports = Product;
+module.exports = { Product, validateProduct, productSchema };
--- routes/products.js
+++ routes/products.js
@@ -1,17 +1,77 @@
-const Product = require("../models/product");
+const { Product, validateProduct } = require("../models/product");
 const express = require("express");
 const { append } = require("express/lib/response");
 const router = express.Router();
 
 // ENDPOINTS //
-router.post("/", async (req,res) => {
-    try {
-        let newProduct = await new Product(req.body);
-        await newProduct.save();
-        return res.status(201).send(newProduct);
-    } catch (error) {
-        return res.status(500).send(`Internal Server Error: ${error}`);
-    }
+router.post("/", async (req, res) => {
+  try {
+    const { error } = validateProduct(req.body);
+    if (error) return res.status(400).send(error);
+
+    let newProduct = await new Product(req.body);
+    await newProduct.save();
+
+    return res.status(201).send(newProduct);
+  } catch (error) {
+    return res.status(500).send(`Internal Server Error: ${error}`);
+  }
+});
+
+router.get("/", async (req, res) => {
+  try {
+    let products = await Product.find();
+    if (!products)
+      return res.status(400).send(`No products in this collection.`);
+    return res.status(200).send(products);
+  } catch (error) {
+    return res.status(500).send(`Internal Server Error: ${error}`);
+  }
+});
+
+router.get("/:productId", async (req, res) => {
+  try {
+    let product = await Product.findById(req.params.productId);
+    if (!product)
+      return res
+        .status(400)
+        .send(`Product with ObjectID ${req.params.productId} does not exist!`);
+    return res.status(200).send(product);
+  } catch (error) {
+    return res.status(500).send(`Internal Server Error: ${error}`);
+  }
+});
+
+router.put("/:productId", async (req, res) => {
+  try {
+    const { error } = validateProduct(req.body);
+    if (error) return res.status(400).send(error);
+    let product = await Product.findByIdAndUpdate(
+      req.params.productId,
+      req.body,
+      { new: true }
+    );
+    if (!product)
+      return res
+        .status(400)
+        .send(`Product with ObjectID ${req.params.productId} does not exist!`);
+    return res.send(product);
+  } catch (error) {
+    return res.status(500).send(`Internal Server Error: ${error}`);
+  }
+});
+
+router.delete("/:productId", async (req, res) => {
+  try {
+    let product = await Product.findByIdAndDelete(req.params.productId);
+    if (!product)
+      return res
+        .status(400)
+        .send(`Product with ObjectID ${req.params.productId} does not exist!`);
+    return res.send(product);
+  } catch (error) {
+    return res.status(500).send(`Internal Server Error: ${error}`);
+  }
 });
 
 module.exports = router;
